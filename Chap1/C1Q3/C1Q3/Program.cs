0adb6eb88138f90587d814af180169d57b62c839
Kawamata Akira AK <autumn@piedey.co.jp>
2017-08-14 11:47:13 +0900

    add c1q3q4

---
+++ Chap1/C1Q3/C1Q3/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("誌伊社符太郎");
+    }
+}
+
---
+++ Chap1/C1Q4/C1Q4/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("{1}は{0}番目の惑星です。", ???);
+    }
+}
