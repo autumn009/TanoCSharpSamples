6baeb78e94f3724715d70e8bdfe05bf5e8431eb5
Kawamata Akira AK <autumn@piedey.co.jp>
2017-08-14 11:14:24 +0900

    add c1q1q2

---
+++ Chap1/C1Q1/C1Q1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        int b = 2;
+        a = b;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+}
---
+++ Chap1/C1Q2/C1Q2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b;
+        a = b = 123;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+}
