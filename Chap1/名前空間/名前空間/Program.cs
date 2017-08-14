aeefc70aed5a3ef34027d3c30a62e0401976cc41
Kawamata Akira AK <autumn@piedey.co.jp>
2017-08-14 14:43:15 +0900

    add namespace

---
+++ Chap1/名前空間/名前空間/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+        }
+    }
+}
