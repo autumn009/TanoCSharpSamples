ed4b2e93090cd43a536d0d3a39e739b3aee06479
Kawamata Akira AK <autumn@piedey.co.jp>
2017-08-14 10:44:52 +0900

    add cond

---
+++ Chap1/代入できる条件/代入できる条件/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 123;
+        long b;
+        b = a;
+        Console.WriteLine(b);
+    }
+}
---
+++ Chap1/代入できる条件2/代入できる条件2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 123;
+        string b;
+        b = a;
+        //b = a.ToString();
+        Console.WriteLine(b);
+    }
+}
