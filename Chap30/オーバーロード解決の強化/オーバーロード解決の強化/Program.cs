dcc96b914a6257f47796a81a44c91276f4cc5bda
Kawamata Akira AK <autumn@piedey.co.jp>
2017-08-15 21:06:12 +0900

    add some

---
+++ Chap30/オーバーロード解決の強化/オーバーロード解決の強化/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+class Program
+{
+    static Task DoThings()
+    {
+        return Task.FromResult(0);
+    }
+    static void Main(string[] args)
+    {
+        Task.Run(DoThings);
+    }
+}
---
+++ Chap30/コレクション初期化での拡張メソッド/コレクション初期化での拡張メソッド/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class A : IEnumerable<int>
+{
+    IEnumerator<int> IEnumerable<int>.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+static class S
+{
+    public static void Add(this A a, int x)
+    {
+        Console.WriteLine("Added:" + x);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var a = new A() { 111, 222, 333, 444, 555, 666, 777, 888, 999 };
+    }
+}
