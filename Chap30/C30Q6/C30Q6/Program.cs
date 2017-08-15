ca44470a9f7d990df3401569e908a1d3910c5c4c
Kawamata Akira AK <autumn@piedey.co.jp>
2017-08-15 18:07:54 +0900

    add some

---
+++ Chap30/C30Q6/C30Q6/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"{true ? 1:2}");
+    }
+}
---
+++ Chap30/文字列の書き入れ/文字列の書き入れ/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var x = 1;
+        var y = 2;
+        var a = string.Format("{0},{1}", x, y);
+        var b = $"{x},{y}";
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+}
