f5c516285ae09f5f3df0d0d054b404e604f07427
Kawamata Akira AK <autumn@piedey.co.jp>
2017-08-15 20:48:23 +0900

    add some

---
+++ Chap30/C30Q10/C30Q10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new int[] { 1, 2, 3 }.ToList();
+        foreach (var item in list) Console.WriteLine(item);
+    }
+}
---
+++ Chap30/インデックス初期化子/インデックス初期化子/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> names1 = new List<string>
+            {
+                "One",
+                "Two",
+                "Three"
+            };
+        Dictionary<int, string> names2 = new Dictionary<int, string>
+        {
+            [1] = "Ken",
+            [2] = "Joe",
+            [3] = "Jun"
+        };
+        foreach (var item in names1) Console.WriteLine(item);
+        foreach (var item in names2.Keys) Console.WriteLine("{0}={1}", item, names2[item]);
+    }
+}
+
+
