9eab165e23f39f27ba1dcf4167ee89a79e4cfeb1
Kawamata Akira AK <autumn@piedey.co.jp>
2017-08-16 13:56:33 +0900

    add some

---
+++ Chap31/C31Q1/C31Q1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (int.TryParse("123", out int r)) Console.WriteLine(r);
+        Console.WriteLine(r);
+    }
+}
---
+++ Chap31/out変数/out変数/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // C# ～6.0
+        int r1;
+        if (int.TryParse("123", out r1)) Console.WriteLine(r1);
+
+        // C# 7.0～
+        if (int.TryParse("123", out int r2)) Console.WriteLine(r2);
+    }
+}
