959532361ee4214b0cf3e39492b560349cba91ab
Kawamata Akira AK <autumn@piedey.co.jp>
2017-08-21 17:01:11 +0900

    update s

---
+++ Chap29/こんなプログラムを作ってみよう/こんなプログラムを作ってみよう/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+class Program
+{
+    private static string[] GetData()
+    {
+        var list = new List<string>();
+        for (int i = 0; i < 10; i++)
+        {
+            list.Add(i.ToString());
+            Task.Delay(1000).Wait();
+        }
+        return list.ToArray();
+    }
+    static void Main(string[] args)
+    {
+        var start = DateTime.Now;
+        Console.WriteLine(GetData().First());
+        Console.WriteLine(DateTime.Now - start);
+    }
+}
