1e5074c37649fd787661be70c1a8837f14d87490
Kawamata Akira AK <autumn@piedey.co.jp>
2017-08-28 18:01:37 +0900

    update s

---
+++ Chap2/値を返すラムダ式/値を返すラムダ式/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte a = 2;
+        double b = 3.0;
+        int c;
+        Func<byte, double, int> f = (x, y) =>
+        {
+            return (int)(x * y);
+        };
+        c = f(a, b);
+        Console.WriteLine(c);
+    }
+}
---
+++ Chap2/引数リストのバリエーション/引数リストのバリエーション/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Func<int, int> x1 = (a) => a * 3; // ①
+        Func<int, int> x2 = (int a) => a * 3; // ②
+        Func<int, int> x3 = a => a * 3; // ③
+        Console.WriteLine(x1(2));
+        Console.WriteLine(x2(2));
+        Console.WriteLine(x3(2));
+    }
+}
