676a0b6b55251816014a202df82b4cd9eaa00788
Kawamata Akira AK <autumn@piedey.co.jp>
2017-09-02 11:34:16 +0900

    update s

---
+++ Chap5/値型の値渡しと参照型の値渡しの違い/値型の値渡しと参照型の値渡しの違い/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class A
+//struct A
+{
+    internal int X;
+}
+
+class Program
+{
+    private static void sample(A a)
+    {
+        a.X = 2;
+    }
+    static void Main(string[] args)
+    {
+        var a = new A() { X = 1 };
+        sample(a);
+        Console.WriteLine(a.X);
+    }
+}
---
+++ Chap5/参照渡しの基本2/参照渡しの基本2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    private static void twotimes(int a, out int b)
+    {
+        b = a * 2;
+    }
+
+    static void Main(string[] args)
+    {
+        int r;
+        twotimes(2, out r);
+        Console.WriteLine(r);
+    }
+}
