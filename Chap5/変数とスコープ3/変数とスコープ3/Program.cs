7427b3db50c1c368538c9a1837ca9b6bf61d04de
Kawamata Akira AK <autumn@piedey.co.jp>
2017-09-03 20:12:19 +0900

    update s

---
+++ Chap5/変数とスコープ3/変数とスコープ3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        {
+            var a = 1;
+        }
+        Console.WriteLine(a);
+    }
+}
---
+++ Chap5/変数の寿命/変数の寿命/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        alpha = 1;  // 変数を利用する
+        int alpha;  // ここで変数誕生
+        Console.WriteLine();
+    }
+}
