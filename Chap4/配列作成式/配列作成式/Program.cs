72cad303f0c853098961b66850b101dc55bafbab
Kawamata Akira AK <autumn@piedey.co.jp>
2017-09-03 21:20:52 +0900

    update s

--- Chap4/配列作成式/配列作成式/Program.cs
+++ Chap4/配列作成式/配列作成式/Program.cs
@@ -4,8 +4,11 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var array = new int[10];
-        for (int i = 0; i < array.Length; i++) array[i] = i * 2;
+        var array = new int[4];
+        array[0] = 0;
+        array[1] = 2;
+        array[2] = 4;
+        array[3] = 6;
         foreach (var item in array) Console.Write(item);
     }
 }
---
+++ Chap5/1を足す・1を引く短縮表記/1を足す・1を引く短縮表記/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        Console.WriteLine(a);
+        a++;
+        Console.WriteLine(a);
+        a--;
+        Console.WriteLine(a);
+    }
+}
+
