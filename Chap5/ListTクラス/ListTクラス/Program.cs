eda59aa2b9f03bbf4d59d70abf4893518768ed50
Kawamata Akira AK <autumn@piedey.co.jp>
2017-09-04 15:43:20 +0900

    update s

---
+++ Chap5/ListTクラス/ListTクラス/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] tokuten = new int[2]; // テストを受けたのは二人だ
+        tokuten[0] = 80;    // たろうくんは80点
+        tokuten[1] = 65;    // じろうくんは65点
+        //tokuten[2] = 95;    // 遅刻したさぶろうくんは95点
+        Console.WriteLine(tokuten.Sum());
+    }
+}
+
---
+++ Chap5/ListTクラス3/ListTクラス3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var tokuten = new List<int>();
+        tokuten.Add(80);    // たろうくんは80点
+        tokuten.Add(65);    // じろうくんは65点
+        tokuten.Add(95);    // 遅刻したさぶろうくんは95点
+        Console.WriteLine(tokuten.Sum());
+    }
+}
