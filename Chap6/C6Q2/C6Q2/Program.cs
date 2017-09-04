90d724a7a383a5203772a9e6d0ffa3e1e170be73
Kawamata Akira AK <autumn@piedey.co.jp>
2017-09-04 18:44:48 +0900

    update s

---
+++ Chap6/C6Q2/C6Q2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        char a = '#';
+        a += '!';
+        Console.WriteLine(a);
+    }
+}
---
+++ Chap6/C6Q3/C6Q3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        char a = '1';
+        Console.WriteLine(a);   // 結果は1だと思った者の答え
+        int n = a;
+        Console.WriteLine(n);  // 結果は49だと思った者の答え
+    }
+}
