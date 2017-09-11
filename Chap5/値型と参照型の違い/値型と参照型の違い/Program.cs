e3b1245912d4a88837a653c86fe4143e486d267f
Kawamata Akira AK <autumn@piedey.co.jp>
2017-09-11 11:18:32 +0900

    update s

--- Chap5/値型と参照型の違い/値型と参照型の違い/Program.cs
+++ Chap5/値型と参照型の違い/値型と参照型の違い/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 
-public class A
+public class MyReferenceType
+{
+    public int X;
+}
+
+public struct MyValueType
 {
     public int X;
 }
@@ -9,19 +14,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        // class A
-        A a1 = new A(); // 変数を宣言
+        // class MyreferenceType
+        MyReferenceType a1 = new MyReferenceType(); // 変数を宣言
         a1.X = 123; // 123を入れる
-        A a2 = a1;  // 複製を作る
+        MyReferenceType a2 = a1;  // 複製を作る
         a1.X = 456; // 456を入れる
         Console.WriteLine("a1.X={0}", a1.X);
         Console.WriteLine("a2.X={0}", a2.X);
-        // int
-        int b1; // 変数を宣言
-        b1 = 123; // 123を入れる
-        int b2 = b1;  // 複製を作る
-        b1 = 456; // 456を入れる
-        Console.WriteLine("b1={0}", b1);
-        Console.WriteLine("b2={0}", b2);
+        // struct MyValueType
+        MyValueType b1 = new MyValueType(); // 変数を宣言
+        b1.X = 123; // 123を入れる
+        MyValueType b2 = b1;  // 複製を作る
+        b1.X = 456; // 456を入れる
+        Console.WriteLine("b1.X={0}", b1.X);
+        Console.WriteLine("b2.X={0}", b2.X);
     }
 }
