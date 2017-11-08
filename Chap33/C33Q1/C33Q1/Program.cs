bb8f08811d51bccc3a1a1f5e7e567dbcee4bdbca
Kawamata Akira AK <autumn@piedey.co.jp>
2017-11-08 14:44:33 +0900

    update s

---
+++ Chap33/C33Q1/C33Q1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+struct X
+{
+    internal int Y;
+}
+
+class Program
+{
+    private static void sub(in X x)
+    {
+        Console.WriteLine(x.Y++);
+    }
+    static void Main(string[] args)
+    {
+        var a = new X();
+        a.Y = 123;
+        sub(in a);
+    }
+}
