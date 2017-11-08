4659c951f13f861e5b1308311463851df52008eb
Kawamata Akira AK <autumn@piedey.co.jp>
2017-11-08 19:51:05 +0900

    update s

---
+++ Chap33/C33Q5/C33Q5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    private static ref T choice<T>(bool condition, ref T consequence, ref T alternative)
+    {
+        if (condition)
+            return ref consequence;
+        else
+            return ref alternative;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] ar = { 1, 2, 3 };
+        int[] ar2 = { 1, 2, 3 };
+        ref var x = ref choice(ar != null, ref ar[0], ref ar2[0]);
+        x = 4;
+        foreach (var item in ar) Console.Write($"{item} ");
+    }
+}
---
+++ Chap33/先頭に書かれた桁のセパレーター文字/先頭に書かれた桁のセパレーター文字/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(0x1_0);   // OK in C# 7.0
+        Console.WriteLine(0x_1_0);  // OK in C# 7.2
+    }
+}
