5053c2b86819fb7fc112dcca0b796d7c32820026
Kawamata Akira AK <autumn@piedey.co.jp>
2017-11-08 16:55:21 +0900

    update s

---
+++ Chap33/C33Q2/C33Q2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+readonly struct Example
+{
+    public int MyProperty1 { get; }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var e = new Example();
+        Console.WriteLine(e.MyProperty1);
+    }
+}
---
+++ Chap33/読み出し専用の構造体/読み出し専用の構造体/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+readonly struct Example
+{
+    internal readonly int MyField1;
+    // 許可されない
+    // internal int MyField2;
+    public int MyProperty1 { get; }
+    // 許可されない
+    // public int MyProperty2 { get; set; }
+    internal void Sub()
+    {
+        int x = 123;
+        // 許可されない
+        // MyField1 = 123;
+    }
+    internal Example(int x, int y)
+    {
+        MyField1 = x;
+        MyProperty1 = y;
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var e = new Example(1,2);
+        e.Sub();
+        Console.WriteLine(e.MyField1);
+        Console.WriteLine(e.MyProperty1);
+    }
+}
