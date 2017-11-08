e7a9891c3c23a6fdbccb7bfc847dfcc635933c67
Kawamata Akira AK <autumn@piedey.co.jp>
2017-11-08 17:57:49 +0900

    update s

---
+++ Chap33/アクセシビリティprivate protected/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Class1
+{
+    private protected int myProperty1 { get; set; }
+}
+
+public class Class2 : Class1
+{
+    public void Sub()
+    {
+        Console.WriteLine(myProperty1);
+    }
+}
---
+++ Chap33/アクセシビリティprivate protected/アクセシビリティprivate protected/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Class3 : Class2
+{
+    public void Sub2()
+    {
+        Sub();
+        // 以下はコンパイルエラーになる
+        // Console.WriteLine(myProperty1);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var x = new Class3();
+        x.Sub2();
+    }
+}
