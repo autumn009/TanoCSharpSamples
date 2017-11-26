8b865ab58f1e5075c65f00734de3c98aea1ee768
Kawamata Akira AK <autumn@piedey.co.jp>
2017-11-26 17:29:11 +0900

    update s

---
+++ Chap33/参照安全な構造体/参照安全な構造体/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+ref struct A {
+    internal int TheValue { get; set; }
+}
+
+ref struct B
+{
+    internal A TheStruct { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var x = new B();
+        Console.WriteLine(x.TheStruct.TheValue);
+    }
+}
