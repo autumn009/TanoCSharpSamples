e341014f6a7556f6e24758688b5e5fbdd6594001
Kawamata Akira AK <autumn@piedey.co.jp>
2017-12-19 19:33:02 +0900

    update s

---
+++ Chap33/条件演算子と参照/条件演算子と参照/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] ar = { 1, 2, 3 };
+        ref var x = ref (ar[0] == 0 ? ref ar[1] : ref ar[2]);
+        x = 4;
+        foreach (var item in ar) Console.Write($"{item} ");
+    }
+}
