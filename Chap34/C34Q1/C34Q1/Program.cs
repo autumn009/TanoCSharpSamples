0e91bc24783e8cc6837dd7761fd4dc5aaef10880
Akira Kawamata <autumn@piedey.co.jp>
2018-04-10 12:07:23 +0900

    updates

---
+++ Chap34/C34Q1/C34Q1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class A<T> where T : unmanaged
+{
+    public static void DoIt() => Console.WriteLine(typeof(T).Name);
+}
+
+class Program
+{
+    static void Main()
+    {
+        A<int>.DoIt();
+        A<string>.DoIt();
+        A<Int32>.DoIt();
+        A<DateTime>.DoIt();
+    }
+}
---
+++ Chap34/列挙型などの制約の強化/列挙型などの制約の強化/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class A<T> where T : System.Enum
+{
+    public static void DoIt()
+    {
+        foreach (var item in Enum.GetNames(typeof(T))) Console.Write(item);
+    }
+}
+
+enum E { X, Y, Z }
+
+class Program
+{
+    static void Main() => A<E>.DoIt();
+}
+
