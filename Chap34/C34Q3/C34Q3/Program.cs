d0ab873e0770b5718d83940693550f1918b3692e
Akira Kawamata <autumn@piedey.co.jp>
2018-04-10 14:38:20 +0900

    update s

---
+++ Chap34/C34Q3/C34Q3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Span<int> x = stackalloc[] { 1, 2, 3 };
+        Action t = () => Console.WriteLine(x[0]);
+        t();
+    }
+}
---
+++ Chap34/スタックへの配列確保/スタックへの配列確保/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Span<int> x = stackalloc[] { 1, 2, 3 };
+        foreach (var item in x) Console.Write(item);
+    }
+}
