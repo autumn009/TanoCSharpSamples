8d17337a324fac55ecb894e2293b67b8844ffaaf
Akira Kawamata <autumn@piedey.co.jp>
2018-04-10 16:03:50 +0900

    update s

---
+++ Chap34/自動実装プロパティのフィールドに属性/自動実装プロパティのフィールドに属性/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text;
+
+[Serializable]
+class X
+{
+    [field: NonSerialized]
+    public int x { get; set; }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        X obj = new X();
+        IFormatter formatter = new SoapFormatter();
+        var stream = new MemoryStream();
+        formatter.Serialize(stream, obj);
+        Console.Write(Encoding.UTF8.GetString(stream.ToArray()));
+    }
+}
