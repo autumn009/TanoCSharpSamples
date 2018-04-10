a639dff20f0fd0edeed3ea57f02ef113a8dfd3bd
Akira Kawamata <autumn@piedey.co.jp>
2018-04-10 13:42:23 +0900

    update s

---
+++ Chap34/C34Q2/C34Q2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var a = 1;
+        var b = 2;
+        var c = 3;
+        ref var x = ref a;
+        x = ref b;
+        x = c;
+        Console.Write($"a={a},b={b},c={c},x={x}");
+    }
+}
---
+++ Chap34/ローカルの参照への再代入/ローカルの参照への再代入/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var a = 1234;
+        var b = 5678;
+        ref var c = ref a;
+        Console.WriteLine(c);
+        c = ref b;
+        Console.WriteLine(c);
+    }
+}
