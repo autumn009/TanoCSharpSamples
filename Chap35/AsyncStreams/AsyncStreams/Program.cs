e7018abe2f37404fb497a9841715084cea34d2dc
Akira Kawamata <autumn@piedey.co.jp>
2018-12-05 17:24:05 +0900

    add AsyncStreams

---
+++ Chap35/AsyncStreams/AsyncStreams/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    const string str = @"Alpha
+Beta
+Gamma
+";
+    static async IAsyncEnumerable<string> sub()
+    {
+        var reader = new StringReader(str);
+        for (; ; )
+        {
+            var s = await reader.ReadLineAsync();
+            if (s == null) break;
+            yield return s;
+        }
+    }
+
+    static async Task Main()
+    {
+        await foreach (var item in sub())
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
