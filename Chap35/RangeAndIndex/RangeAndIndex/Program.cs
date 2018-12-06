85de3b375d381c913170c90264a2b163302e80a3
Akira Kawamata <autumn@piedey.co.jp>
2018-12-06 10:55:32 +0900

    re-write Range sample

--- Chap35/RangeAndIndex/RangeAndIndex/Program.cs
+++ Chap35/RangeAndIndex/RangeAndIndex/Program.cs
@@ -2,37 +2,30 @@
 
 class Program
 {
-    static void Main(string[] args)
+    private static char[] ar = { 'P', 'E', 'N', 'D', 'U', 'L', 'U', 'M' };
+    private static void sub(string label, Range range)
     {
-        string[] ar =
-        {
-            "Windows",
-            "Hello",
-            "World",
-            "Series",
-            "End"
-        };
+        Console.Write($"{label}: ");
+        var r1 = ar[range];
+        foreach (var item in r1) Console.Write(item);
+        Console.WriteLine();
+    }
 
+    static void Main()
+    {
         Index i1 = 1;
         Index i2 = 3;
         Index i3 = ^2;
-        // インデックスがi1の値
-        Console.WriteLine(ar[i1]);
-        // インデックスがi2の値
-        Console.WriteLine(ar[i2]);
-        // インデックスがi3の値
-        Console.WriteLine(ar[i3]);
-        // インデックスが1～3の範囲
-        var r1 = ar[1..3];
-        foreach (var item in r1) Console.Write(item);
-        Console.WriteLine();
-        // インデックスがi1～i2の範囲(i2は含まない)
-        var r2 = ar[i1..i2];
-        foreach (var item in r2) Console.Write(item);
-        Console.WriteLine();
-        // インデックスがi1～i3の範囲(i3は含まない)
-        var r3 = ar[i1..i3];
-        foreach (var item in r3) Console.Write(item);
-        Console.WriteLine();
+        Console.WriteLine($"インデックスがi1(1)の値: {ar[i1]}");
+        Console.WriteLine($"インデックスがi2(3)の値: {ar[i2]}");
+        Console.WriteLine($"インデックスがi3(^2)の値: {ar[i3]}");
+
+        sub("インデックスが1～3の範囲", 1..3);
+        sub("インデックスがi1(1)～i2(3)の範囲(i2は含まない)", i1..i2);
+        sub("インデックスがi1(1)～i3(^2)の範囲(i3は含まない)", i1..i3);
+        sub("インデックスが2～の範囲", 2..);
+        sub("インデックスが～3の範囲", ..3);
+        sub("インデックスが～^3の範囲", ..^3);
+        sub("インデックスが完全オープン(..)の範囲", ..);
     }
 }
