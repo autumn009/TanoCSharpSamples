6828f1a6dfd0b2c0a2a76e6c828031cb858d5b1c
Akira Kawamata <autumn@piedey.co.jp>
2018-12-12 12:03:03 +0900

    add Null Coalescing Assignment

---
+++ Chap35/NullCoalescingAssignment/NullCoalescingAssignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = null;
+        // old style
+        s = s ?? "s is null";
+        // C# 8.0 style
+        s ??= "s is null";
+        Console.WriteLine(s);
+    }
+}
