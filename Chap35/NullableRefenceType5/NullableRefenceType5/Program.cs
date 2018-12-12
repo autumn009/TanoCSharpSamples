f8fb57557b99dc29f0761129ab28f004283976a9
Akira Kawamata <autumn@piedey.co.jp>
2018-12-12 11:21:24 +0900

    add NullableRefenceType5

---
+++ Chap35/NullableRefenceType5/NullableRefenceType5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+#nullable enable
+
+class Program
+{
+    private static void sub(ref string s, string n)
+    {
+        if (int.TryParse(n, out int r)) s += n;
+        else s = "ERROR";
+    }
+
+    static void Main()
+    {
+        string s1 = null!;
+        sub(ref s1, "ABC");
+        Console.WriteLine(s1);
+    }
+}
