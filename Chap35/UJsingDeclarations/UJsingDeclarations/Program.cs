b502c63ea63425d815119cb5849ddb52b8fe0574
Akira Kawamata <autumn@piedey.co.jp>
2019-02-28 15:37:14 +0900

    add UsingDeclarations

---
+++ Chap35/UJsingDeclarations/UJsingDeclarations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var s = "One\r\nTwo\r\nThree";
+        // old pattern
+        using (var reader1 = new StringReader(s))
+        {
+            Console.WriteLine(reader1.ReadToEnd());
+        }
+        // new pattern
+        using var reader2 = new StringReader(s);
+        Console.WriteLine(reader2.ReadToEnd());
+    }
+}
