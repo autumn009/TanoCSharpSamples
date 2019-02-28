fccb9935c970fc7c3dc28134bbfc1d7dcbd97ba9
Akira Kawamata <autumn@piedey.co.jp>
2019-02-28 14:39:33 +0900

    add SwitchExpression

---
+++ Chap35/SwitchExpressions/SwitchExpressions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void sub(object o)
+    {
+        Console.WriteLine(o switch {
+        string s when s.Length < 3 => s.ToUpper(),
+        string s => s.ToLower(),
+        _ => "It's other"
+        });
+    }
+
+
+    static void Main()
+    {
+        sub("Hello");
+        sub("Ok");
+        sub(null);
+    }
+}
