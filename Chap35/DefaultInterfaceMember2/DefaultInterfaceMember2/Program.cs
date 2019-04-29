75ba9209790348011184c39ceac12b138904efca
Akira Kawamata <autumn@piedey.co.jp>
2019-04-29 16:37:51 +0900

    add DefaultInterfaceMember2

---
+++ Chap35/DefaultInterfaceMember2/DefaultInterfaceMember2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public readonly struct Sample
+{
+    public readonly int x;
+    public readonly void Write() => Console.WriteLine(x);
+    public Sample(int x) => this.x = x;
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        var a = new Sample(4649);
+        a.Write();
+    }
+}
