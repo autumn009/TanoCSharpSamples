2747355df7b02705be391cc99cd52731910761d8
Akira Kawamata <autumn@piedey.co.jp>
2019-04-29 16:25:29 +0900

    add ReadonlyInstanceMembers

---
+++ Chap35/ReadonlyInstanceMembers/ReadonlyInstanceMembers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public struct Sample
+{
+    public int X { get; set; }
+    readonly public void SubOK()
+    {
+        Console.WriteLine(X+1);
+    }
+    //readonly public void SubFail()
+    //{
+    //  X++;
+    //  Console.WriteLine(X);
+    //}
+}
+
+class Program
+{
+    static void Main()
+    {
+        var a = new Sample();
+        a.X = 1;
+        a.SubOK();
+    }
+}
