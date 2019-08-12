fde45c157c9371c9b106c288aef23954aeb421f2
Akira Kawamata <autumn@piedey.co.jp>
2019-08-12 11:50:57 +0900

    add MayBeNullWhen

---
+++ Chap35/MayBeNullWhenAndNotNullWhen/MayBeNullWhenAndNotNullWhen/Program.cs
@@ -0,0 +1,33 @@
+﻿#nullable enable
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    private static bool tryParseClassName(char ch, [NotNullWhen(true)] out string? name)
+    {
+        if (ch == 'Y')
+        {
+            name = "3年Y組ニャンパチ先生";
+            return true;
+        }
+        name = null;
+        return false;
+    }
+
+    private static void report(string s)
+    {
+        Console.WriteLine(s);
+    }
+
+    static void Main()
+    {
+        if (tryParseClassName('Y', out string? r))
+        {
+            report(r);
+            return;
+        }
+        //report(r);
+        Console.WriteLine("Unknown Class");
+    }
+}
