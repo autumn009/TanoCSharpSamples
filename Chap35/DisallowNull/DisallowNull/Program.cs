011c86fa130a48fa076e1ad611b797a3f7033d92
Akira Kawamata <autumn@piedey.co.jp>
2019-08-12 10:58:05 +0900

    add DisallowNull

---
+++ Chap35/DisallowNull/DisallowNull/Program.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    private static string? s = string.Empty;
+    [DisallowNull]
+    public static string? MyProperty
+    {
+        get { return s; }
+        set { s = value; }
+    }
+
+    public static void ShowStringLength(string s)
+    {
+        Console.WriteLine(s.Length);
+    }
+    static void Main(string[] args)
+    {
+        // null リテラルを null 非許容参照型に変換できません。
+        MyProperty = null;
+        // パラメーター 's' に Null 参照引数がある可能性があります
+        ShowStringLength(MyProperty);
+    }
+}
