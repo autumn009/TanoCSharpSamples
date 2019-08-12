88317d2c342e84a59f140706c7de2ce0e0a40839
Akira Kawamata <autumn@piedey.co.jp>
2019-08-12 14:48:20 +0900

    add DoesNotReturn

---
+++ Chap35/DoesNotReturn/DoesNotReturn/Program.cs
@@ -0,0 +1,28 @@
+﻿// Thus source code was not working in Visual Studio 2019 16.3.0 Preview 1.0
+
+#nullable enable
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    [DoesNotReturn]
+    private static void ReportError(bool errorType)
+    {
+        if (errorType)
+        {
+            throw new ArgumentException("Wrong Argument!");
+        }
+        else
+        {
+            throw new DivideByZeroException("It was ZERO!");
+        }
+    }
+
+    static void Main()
+    {
+        string s;
+        ReportError(false);
+        Console.WriteLine(s);
+    }
+}
