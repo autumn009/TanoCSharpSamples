86485df5e36810da014bd23a71298bdb8eb62eb7
Akira Kawamata <autumn@piedey.co.jp>
2019-08-12 09:39:04 +0900

    add NotNull

---
+++ Chap35/NotNull/NotNull/Program.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+using System;
+
+public class A<T> where T:notnull
+{
+    internal int AddHashCode(T t1, T t2)
+    {
+        return t1.GetHashCode() + t2.GetHashCode();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var a = "Hello";
+        var b = "World";
+        var x = new A<string>();
+        Console.WriteLine(x.AddHashCode(a, b));
+        //string? c = null;
+        //var y = new A<string?>();
+        //Console.WriteLine(y.AddHashCode(a, c));
+    }
+}
