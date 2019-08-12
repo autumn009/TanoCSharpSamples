b3a130241dc6263b599de804b1b7592537c5f74b
Akira Kawamata <autumn@piedey.co.jp>
2019-08-12 10:26:26 +0900

    Add AllowNull

---
+++ Chap35/AllowNull/AllowNull/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    private static string s = string.Empty;
+    [AllowNull]
+    public static string MyProperty
+    {
+        get { return s; }
+        set { s = value ?? string.Empty; }
+    }
+
+    public static void ShowStringLength(string s)
+    {
+        Console.WriteLine(s.Length);
+    }
+    static void Main(string[] args)
+    {
+        // safe because [AllowNull]
+        MyProperty = null;
+        // safe becase MyProperty is 'string' type
+        ShowStringLength(MyProperty);
+    }
+}
