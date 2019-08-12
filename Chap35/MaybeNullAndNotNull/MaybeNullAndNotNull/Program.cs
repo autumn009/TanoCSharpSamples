fc5cf29ef784c5002caee3d91ddc278b1717e77b
Akira Kawamata <autumn@piedey.co.jp>
2019-08-12 11:18:52 +0900

    add MaybeNullAndNotNull

---
+++ Chap35/MaybeNullAndNotNull/MaybeNullAndNotNull/Program.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    [return: MaybeNull]
+    private static string? Sub1(bool b)
+    {
+        if (b) return "Hello1"; else return null;
+    }
+
+    [return: NotNull]
+    private static string? Sub2()
+    {
+        return "Hello2";
+    }
+
+    static void Main()
+    {
+        string s = Sub1(true)?.Trim() + Sub2().Trim();
+        Console.WriteLine(s);
+    }
+}
