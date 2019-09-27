2fb5ddb23f7cd08aa63f81cbe8f0c8931c350fb8
Akira Kawamata <autumn@piedey.co.jp>
2019-09-27 11:34:09 +0900

    add VervatimString

---
+++ Chap35/VerbatimStrings/VerbatimStrings/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 120;
+        string b = $@"\{a}-";
+        string c = @$"\{a}-";
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+    }
+}
