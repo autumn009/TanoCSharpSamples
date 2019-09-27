bc0056a6dc09e71ea48b99ed778198e6afdf3941
Akira Kawamata <autumn@piedey.co.jp>
2019-09-27 10:36:52 +0900

    add StaticLocalFunction

---
+++ Chap35/StaticLocalFunction/StaticLocalFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 4, b = 2;
+        Console.WriteLine(calcAve1());
+        Console.WriteLine(calcAve2(a, b));
+
+        int calcAve1()
+        {
+            return (a + b) / 2;
+        }
+        static int calcAve2(int x, int y)
+        {
+            return (x + y) / 2;
+        }
+    }
+}
