6cac35d3f6f786805a5e4481bc5a79e961a07ae9
Akira Kawamata <autumn@piedey.co.jp>
2019-09-27 09:37:54 +0900

    add DisposeRefStruct

---
+++ Chap35/DisposeRefStruct/DisposeRefStruct/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+ref struct Sample
+{
+    public void Dispose()
+    {
+        Console.WriteLine("Disposed");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        using(var s = new Sample())
+        {
+            Console.WriteLine("Now working 'ref struct Sample!'");
+        }
+    }
+}
