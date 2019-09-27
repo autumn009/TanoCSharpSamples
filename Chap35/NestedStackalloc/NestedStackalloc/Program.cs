68512724fcbe27e2b34240152963618a0bde9aab
Akira Kawamata <autumn@piedey.co.jp>
2019-09-27 10:56:05 +0900

    add NestedStackalloc

---
+++ Chap35/NestedStackalloc/NestedStackalloc/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Span<int> numbers = stackalloc[] { 1, 3, 5, 7 };
+        var index = numbers.IndexOfAny(stackalloc[] { 4, 5, 6 });
+        Console.WriteLine(index);
+    }
+}
