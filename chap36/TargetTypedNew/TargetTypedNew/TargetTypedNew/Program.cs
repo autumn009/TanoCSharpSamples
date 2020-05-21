1699dfd8102ebbdf1499307181a2ef1a4d6391c6
Akira Kawamata <autumn@piedey.co.jp>
2020-05-21 19:49:33 +0900

    add TargetTypedNew

---
+++ chap36/TargetTypedNew/TargetTypedNew/TargetTypedNew/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // C# 初期のスタイル
+        string s1 = new string('A', 10);
+        // C# 8までのスタイル
+        var s2 = new string('A', 10);
+        // C# 9の新しいスタイル
+        string s3 = new ('A', 10);
+
+        Console.WriteLine(s1);
+        Console.WriteLine(s2);
+        Console.WriteLine(s3);
+    }
+}
