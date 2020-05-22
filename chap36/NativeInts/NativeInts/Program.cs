c11ba215a991c88352ed8a310a221a88ce31af72
Akira Kawamata <autumn@piedey.co.jp>
2020-05-22 10:47:30 +0900

    add NativeInts

---
+++ chap36/NativeInts/NativeInts/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // ↓数値で初期化できない
+        IntPtr oldType = new IntPtr(0);
+        // ↓数値で初期化できる
+        nint newType = 0;
+
+        // ↓できない
+        //oldType++;
+        // ↓できる
+        newType++;
+
+        // ↓できる
+        Console.WriteLine(IntPtr.MaxValue);
+        // ↓できない
+        //Console.WriteLine(nint.MaxValue);
+
+        // 値の型名を教えて
+        Console.WriteLine(oldType.GetType().Name);
+        Console.WriteLine(newType.GetType().Name);
+    }
+}
