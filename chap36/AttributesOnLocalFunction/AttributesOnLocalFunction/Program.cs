7635a674924e3faf6f85e0979035edb2aac7da43
Akira Kawamata <autumn@piedey.co.jp>
2020-06-14 19:29:34 +0900

    add AttributesOnLocalFunction

---
+++ chap36/AttributesOnLocalFunction/AttributesOnLocalFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿    using System;
+
+    public class SampleAttribute : Attribute
+    {
+        public SampleAttribute() { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LocalFunc(1234);
+            static void LocalFunc([Sample] int x)
+            {
+                Console.WriteLine($"Hello in LocalFunc {x}");
+            }
+        }
+    }
