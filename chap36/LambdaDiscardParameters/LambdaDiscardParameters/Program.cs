53bda9a7388b4bef4a00d863ea2592a3e5684e7b
Akira Kawamata <autumn@piedey.co.jp>
2020-06-14 19:20:07 +0900

    add LabdaDiscardParameters and SkipLocaInit

---
+++ chap36/LambdaDiscardParameters/LambdaDiscardParameters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void test(Action<int> act)
+    {
+        act(1234);
+    }
+    static void Main()
+    {
+        test((_) =>
+        {
+            Console.WriteLine("It was called!");
+        });
+    }
+}
---
+++ chap36/SkipLocalsInitAttribute/SkipLocalsInitAttribute/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    [System.Runtime.CompilerServices.SkipLocalsInitAttribute]
+    unsafe static void method()
+    {
+        Span<int> ar = stackalloc int[1];
+        Console.WriteLine(ar[0]);
+    }
+    static void Main()
+    {
+        method();
+    }
+}
