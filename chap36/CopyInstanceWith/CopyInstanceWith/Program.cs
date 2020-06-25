eebdc93038d5ef0b09793e0d075fbb4633b0bff2
Akira Kawamata <autumn@piedey.co.jp>
2020-06-25 11:51:18 +0900

    add CopyInstanceWith

---
+++ chap36/CopyInstanceWith/CopyInstanceWith/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+record Result(string FamilyName, string FirstName, int Point);
+
+class Program
+{
+    public static void Dump(Result r)
+    {
+        Console.WriteLine($"{r.FamilyName}{r.FirstName}は{r.Point}点");
+    }
+    static void Main()
+    {
+        var r = new Result("田中", "花子", 100);
+        Dump(r);
+        var r2 = r with { FamilyName = "斉藤" };
+        Dump(r2);
+        Dump(r);
+    }
+}
