7e5c8c9c43d6bd7a18142208c4eab814f1ed2fdd
Akira Kawamata <autumn@piedey.co.jp>
2020-06-25 11:19:06 +0900

    add TopLevelMain

---
+++ chap36/ExtendedPartialMethod/ExtendedPartialMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+partial class MyClass
+{
+    internal static partial bool TryParse(string s, out int i);
+}
+
+partial class MyClass
+{
+    internal static partial bool TryParse(string s, out int i)
+    {
+        return int.TryParse(s, out i);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int a;
+        MyClass.TryParse("1233", out a);
+        Console.WriteLine(a + 1);
+    }
+}
---
+++ chap36/TopLevelMain/TopLevelMain/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+var t = "Top";
+var l = "Level";
+var s = "Statement";
+
+Console.WriteLine($"{t} {l} {s}");
