69ed32bc479736bc343fdfb690e2d017888ed841
Akira Kawamata <autumn@piedey.co.jp>
2020-06-27 10:55:41 +0900

    add TargetTypedConditionalOperator

---
+++ chap36/TargetTypedConditionalOperator/TargetTypedConditionalOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class A { };
+class B: A { };
+class C : A { };
+
+class Program
+{
+    static void Main()
+    {
+        var b = false;
+        int? x = b ? 1 : null;
+        A y = new B() ?? new C();
+        Console.WriteLine($"{x} {y}");
+    }
+}
