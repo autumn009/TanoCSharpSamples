c9bb612141a185897c9c05e41e7f918da90c0120
Akira Kawamata <autumn@piedey.co.jp>
2020-06-27 10:49:23 +0900

    add FunctionPointer

---
+++ chap36/FunctionPointer/FunctionPointer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+unsafe class Program
+{
+    public static void IndirectCall(delegate*<void> fp)
+    {
+        fp();
+    }
+
+    static void MyMethod()
+    {
+        Console.WriteLine("I'm my method!");
+    }
+    static void Main()
+    {
+        IndirectCall(&MyMethod);
+    }
+}
+
