13d28e8d9e3bddb38fb6e3a1f8b79a675f84a229
Akira Kawamata <autumn@piedey.co.jp>
2020-06-27 11:19:53 +0900

    add CovariantReturs

---
+++ chap36/CovariantReturns/CovariantReturns/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class R1 { }
+class R2 : R1 { }
+
+abstract class A
+{
+    public abstract R1 Sub();
+}
+
+class B : A
+{
+    public override R2 Sub()
+    {
+        Console.WriteLine("B.Sub");
+        return null;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var a = new B();
+        R2 r2 = a.Sub();
+    }
+}
