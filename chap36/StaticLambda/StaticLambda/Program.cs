5ec813058a519771cf6cfef34007b753ba83a275
Akira Kawamata <autumn@piedey.co.jp>
2020-08-27 11:35:22 +0900

    add StaticLambda

---
+++ chap36/StaticLambda/StaticLambda/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 123;
+        Func<int> f1 = () => x;
+        // A static anonymous function cannot contain a reference to 'x'.
+        //Func<int> f2 = static () => x;
+        Func<int,int> f3 = static (p) => p;
+        Console.WriteLine(f1());
+        //Console.WriteLine(f2());
+        Console.WriteLine(f3(x));
+    }
+}
