549aeec253cebd6107fe751aceadcbaf12dfa969
Akira Kawamata <autumn@piedey.co.jp>
2020-09-15 15:15:45 +0900

    add Half

---
+++ chap36/Half/Half/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Half a = (Half)1.0f;
+        Half b = (Half)0.5f;
+        Half c = Half.PositiveInfinity;
+        Half d = Half.NaN;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+    }
+}
