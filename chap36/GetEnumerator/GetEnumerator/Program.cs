55606deb9a0709d33206f6d624240e0896fcb092
Akira Kawamata <autumn@piedey.co.jp>
2020-11-15 08:48:40 +0900

    add GetEnunerator

---
+++ chap36/GetEnumerator/GetEnumerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class X
+{
+    internal int A { get; set; }
+    internal int B { get; set; }
+}
+
+internal static class MyExtensions
+{
+    internal static IEnumerator GetEnumerator(this X x)
+    {
+        yield return x.A;
+        yield return x.B;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var x = new X();
+        foreach (var item in x) Console.WriteLine(item);
+    }
+}
