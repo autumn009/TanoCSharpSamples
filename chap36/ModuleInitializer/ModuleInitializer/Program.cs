0e82e133ae41f0cdcdc8d40b70cbcdfe6569c4f4
Akira Kawamata <autumn@piedey.co.jp>
2020-11-15 08:18:38 +0900

    add ModuleInitializer

---
+++ chap36/ModuleInitializer/ModuleInitializer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    [ModuleInitializerAttribute]
+    public static void ModileInitialized()
+    {
+        Console.WriteLine("Modile Initialized");
+
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
