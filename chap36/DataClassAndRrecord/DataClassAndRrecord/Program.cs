5fbb09293d68c254a809a522590c7e6cb4235b25
Akira Kawamata <autumn@piedey.co.jp>
2020-11-14 16:29:42 +0900

    fix for GA

--- chap36/DataClassAndRrecord/DataClassAndRrecord/Program.cs
+++ chap36/DataClassAndRrecord/DataClassAndRrecord/Program.cs
@@ -1,24 +1,5 @@
 ﻿using System;
 
-// この定義は将来のバージョンで不要になる見込みである
-// エラーになる場合は削除してみると良い
-namespace System.Runtime.CompilerServices
-{
-    internal class IsExternalInit : Attribute { }
-}
-
-// dataキーワードはサンプルソース作成時には機能していない模様である
-#if false
-public data class Result1
-{
-    public string FamilyName { get; init; }
-    public string FirstName { get; init; }
-    public int Point { get; init; }
-}
-
-data class Result2 { string FamilyName; string FirstName; int Point; };
-#endif
-
 record Result3(string FamilyName, string FirstName, int Point);
 
 class Program
