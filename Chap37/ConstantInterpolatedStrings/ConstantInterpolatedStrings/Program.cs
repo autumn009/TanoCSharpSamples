db48f4b4aa04e4625b6e0585d176ff8ea3ac57c3
Akira Kawamata <autumn@piedey.co.jp>
2021-05-30 19:05:35 +0900

    add ConstantInterpolatedString

---
+++ Chap37/ConstantInterpolatedStrings/ConstantInterpolatedStrings/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const string a = "dream";
+        const string b = $"We have new {a}!";
+        Console.WriteLine(b);
+    }
+}
