95d30070be646711fdd9be5fda8c2261d86d8d6c
Akira Kawamata <autumn@piedey.co.jp>
2021-06-17 09:05:36 +0900

    add RecordStruct

---
+++ Chap37/RecordStruct/RecordStruct/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+record struct Sample(int x,int y);
+
+class Program
+{
+    static void Main()
+    {
+        Sample a = new Sample(123,456);
+        Console.WriteLine($"x={a.x} y={a.y} IsValueType={a.GetType().IsValueType}");
+    }
+}
