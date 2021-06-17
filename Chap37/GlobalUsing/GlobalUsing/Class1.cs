01c49176a92dd5f2f9abe8389441ebeb7ce065e5
Akira Kawamata <autumn@piedey.co.jp>
2021-06-17 09:18:46 +0900

    add GlobalUsing

---
+++ Chap37/GlobalUsing/GlobalUsing/Class1.cs
@@ -0,0 +1,10 @@
+﻿global using static System.Console;
+global using System.Linq;
+
+class Class1
+{
+    internal static void Sub()
+    {
+        WriteLine($"Sub {string.Join(',',Enumerable.Range(0,3))}");
+    }
+}
---
+++ Chap37/GlobalUsing/GlobalUsing/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        WriteLine($"Main {string.Join(',', Enumerable.Range(0, 3))}");
+        Class1.Sub();
+    }
+}
