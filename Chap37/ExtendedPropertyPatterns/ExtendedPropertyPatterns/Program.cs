e6d1a5a2fbf91d02a92f950f9e11bc7032ba1835
Akira Kawamata <autumn@piedey.co.jp>
2021-08-14 14:09:37 +0900

    add ExtendedPropertyPatterns

---
+++ Chap37/ExtendedPropertyPatterns/ExtendedPropertyPatterns/Program.cs
@@ -0,0 +1,20 @@
+﻿for (int i = 0; i < 2; i++)
+{
+    var x = new B() { Obj = new A() { Count = i } };
+    var r = x switch
+    {
+        { Obj: { Count: 0 } } => "ZERO",
+        { Obj.Count: 1 } => "ONE",
+        _ => ""
+    };
+    Console.WriteLine(r);
+}
+
+public class A
+{
+    public int Count { get; set; }
+}
+public class B
+{
+    public A? Obj { get; set; }
+}
