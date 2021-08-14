e7d26a41a438215b7d7b2ed1209fd2f185e3ea65
Akira Kawamata <autumn@piedey.co.jp>
2021-08-14 13:44:52 +0900

    add ImprovedDefiniteAssignment

---
+++ Chap37/ImprovedDefiniteAssignment/ImprovedDefiniteAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿MyClass c = new MyClass();
+if ((c != null && c.MyMethod(out object obj1)))
+{
+    // obj1は初期化済みと見抜ける
+    Console.WriteLine(obj1.ToString());
+}
+if ((c != null && c.MyMethod(out object obj2)) == true)
+{
+    // obj2は初期化済みと見抜けない→見抜ける
+    Console.WriteLine(obj2.ToString());
+}
+
+public class MyClass
+{
+    public bool MyMethod(out object obj)
+    {
+        obj = "hello";
+        return true;
+    }
+}
