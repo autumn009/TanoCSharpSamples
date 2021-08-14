49c09fa555222e65f723902dd92d80ec270bdaeb
Akira Kawamata <autumn@piedey.co.jp>
2021-08-14 15:47:46 +0900

    add ParameterlessStructConstructors

---
+++ Chap37/ParameterlessStructConstructors/ParameterlessStructConstructors/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+A a = new A();
+a.Sub();
+
+struct A
+{
+    internal void Sub()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public A()
+    {
+        Console.WriteLine("Constructed!");
+    }
+}
