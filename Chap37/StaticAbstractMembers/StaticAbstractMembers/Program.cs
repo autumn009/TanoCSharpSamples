d2231f9fca36383ac628774270bcce73db03b622
Akira Kawamata <autumn@piedey.co.jp>
2021-08-14 15:29:19 +0900

    add StaticAbstractMembers

---
+++ Chap37/StaticAbstractMembers/StaticAbstractMembers/Program.cs
@@ -0,0 +1,18 @@
+﻿Sub<B>();
+
+void Sub<T>() where T: IA
+{
+    T.MyMethod();
+}
+
+interface IA
+{
+    static abstract void MyMethod();
+}
+class B : IA
+{
+    public static void MyMethod()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
