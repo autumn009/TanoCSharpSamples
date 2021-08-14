39a3bcc34aff68fbb495d5cc05434a8bdd633233
Akira Kawamata <autumn@piedey.co.jp>
2021-08-14 15:04:32 +0900

    add LambdaImprovements

---
+++ Chap37/LambdaImprovements/LambdaImprovements/Program.cs
@@ -0,0 +1,7 @@
+﻿var lambda1 = () => new B();
+var lambda2 = A () => new B();
+Console.WriteLine(lambda1.GetType().FullName);
+Console.WriteLine(lambda2.GetType().FullName);
+
+class A { }
+class B : A { }
