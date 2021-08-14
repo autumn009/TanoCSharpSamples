5957c232e2a714d14d766e8fb25d38cb3114cb2e
Akira Kawamata <autumn@piedey.co.jp>
2021-08-14 15:44:01 +0900

    add FileScopedNamespace

---
+++ Chap37/FileScopedNamespace/FileScopedNamespace/MySample.cs
@@ -0,0 +1,10 @@
+﻿namespace MyNamespace;
+
+class MyClass
+{
+    internal static void Sub()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ Chap37/FileScopedNamespace/FileScopedNamespace/Program.cs
@@ -0,0 +1,1 @@
+﻿MyNamespace.MyClass.Sub();
