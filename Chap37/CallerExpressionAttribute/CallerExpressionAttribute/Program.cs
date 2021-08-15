c44c15b330a629cba2790ef8d563640e57dfc222
Akira Kawamata <autumn@piedey.co.jp>
2021-08-15 14:38:24 +0900

    add CallerExpressionAttribute

---
+++ Chap37/CallerExpressionAttribute/CallerExpressionAttribute/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+CalcAndReport(1.23 * 4.56);
+CalcAndReport((1 + 2) * 3);
+CalcAndReport(Math.PI);
+CalcAndReport("Hello " + "World!");
+
+void CalcAndReport<T>(T argument, [CallerArgumentExpression("argument")] string? argumentExpression = null)
+{
+    Console.WriteLine($"{argumentExpression} = {argument}");
+}
