e5444ecbab00099e61f55ce76ab0298d62532914
Akira Kawamata <autumn@piedey.co.jp>
2021-11-13 10:11:26 +0900

    more simpe

--- Chap37/GlobalUsing/GlobalUsing/Program.cs
+++ Chap37/GlobalUsing/GlobalUsing/Program.cs
@@ -1,10 +1,2 @@
-﻿using System;
-
-class Program
-{
-    static void Main()
-    {
-        WriteLine($"Main {string.Join(',', Enumerable.Range(0, 3))}");
-        Class1.Sub();
-    }
-}
+﻿WriteLine($"Main {string.Join(',', Enumerable.Range(0, 3))}");
+Class1.Sub();
