e29319e0f2b25f88c83d2c5eb9d6c4cd63a24480
Akira Kawamata <autumn@piedey.co.jp>
2022-11-12 20:50:44 +0900

    add RequiredMembers

--- Chap38/RequiredMembers/RequiredMembers/Program.cs
+++ Chap38/RequiredMembers/RequiredMembers/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Person[] persons =
+{
+    new Person(){ Id="aaa", Name="Alice" },
+    new Person(){ Id="bbb" }
+};
+
+sub("aaa");
+sub("bbb");
+
+void sub(string key)
+{
+    var person = persons.First(x => x.Id == key);
+    Console.WriteLine($"Id={person.Id}");
+    Console.WriteLine($"Name={person.Name ?? "unknwon"}");
+}
+
+public class Person
+{
+    public required string Id { get; init; }
+    public string? Name { get; init; }
+}
+
