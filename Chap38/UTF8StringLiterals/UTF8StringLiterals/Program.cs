8779eda0f2fe4fe19f8f3ae5c92567074122f368
Akira Kawamata <autumn@piedey.co.jp>
2022-11-12 14:07:44 +0900

    add UTF8StringLiterals

---
+++ Chap38/UTF8StringLiterals/UTF8StringLiterals/Program.cs
@@ -0,0 +1,12 @@
+﻿string a = "The quick brown fox jumps over the lazy dog";
+byte[] aBytes = System.Text.Encoding.UTF8.GetBytes(a);
+sub(aBytes);
+
+ReadOnlySpan<byte> b = "The quick brown fox jumps over the lazy dog"u8;
+sub(b.ToArray());
+
+void sub(byte[] utf8string)
+{
+    File.WriteAllBytes("thefile.txt",utf8string);
+    Console.WriteLine(File.ReadAllText("thefile.txt"));
+}
