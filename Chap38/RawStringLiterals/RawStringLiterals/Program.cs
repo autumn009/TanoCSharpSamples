961575104cd4dbf6ce009ed655dcf085fa673bbd
Akira Kawamata <autumn@piedey.co.jp>
2022-11-12 14:44:02 +0900

    add RawStringLiterals

---
+++ Chap38/RawStringLiterals/RawStringLiterals/Program.cs
@@ -0,0 +1,23 @@
+﻿string s1 = @"
+<div>
+    <p>
+        This is a XML text.
+    <p>
+</div>
+";
+
+string s2 = """
+    <div>
+        <p>
+            This is a XML text.
+        <p>
+    </div>
+    """;
+
+string s3 = """"
+    """text"""
+    """";
+
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+Console.WriteLine(s3);
