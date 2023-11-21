5a319612130aa9066fef17aa1ff73dcbac3fe6ba
Akira Kawamata <autumn@piedey.co.jp>
2023-11-21 14:04:40 +0900

    add CollectionExpressions

---
+++ Chap39/CollectionExpressions/CollectionExpressions/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] a = [1, 2, 3];
+sub(a);
+sub([4,5,6]);
+int[] b = [.. a, 7, 8, 9];
+sub(b);
+List<int> c = [.. a, .. b, 10, 11, 12];
+sub(c);
+
+static void sub(IEnumerable<int> array) => Console.WriteLine(string.Join(',', array));
