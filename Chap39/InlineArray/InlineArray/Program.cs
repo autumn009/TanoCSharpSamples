550533dcd48506ef78e807c78a2ce1f44f8a9320
Akira Kawamata <autumn@piedey.co.jp>
2023-11-22 19:43:49 +0900

    add InlineArray

---
+++ Chap39/InlineArray/InlineArray/Program.cs
@@ -0,0 +1,12 @@
+﻿var buffer = new CharBuffer();
+for (int i = 0; i < 10; i++)
+{
+    buffer[i] = (char)('a' + i);
+}
+Console.WriteLine(new string(buffer));
+
+[System.Runtime.CompilerServices.InlineArray(10)]
+public struct CharBuffer
+{
+    private char _firstElement;
+}
