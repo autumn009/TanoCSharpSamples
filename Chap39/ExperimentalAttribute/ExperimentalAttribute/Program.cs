e9f22149769d4da0dd72db8ce873a513fd23f25a
Akira Kawamata <autumn@piedey.co.jp>
2023-11-23 07:37:47 +0900

    ExperimentalAttribute

---
+++ Chap39/ExperimentalAttribute/ExperimentalAttribute/Program.cs
@@ -0,0 +1,9 @@
+﻿#pragma warning disable MyID0001 // 種類は、評価の目的でのみ提供されています。将来の更新で変更または削除されることがあります。続行するには、この診断を非表示にします。
+sub();
+#pragma warning restore MyID0001 // 種類は、評価の目的でのみ提供されています。将来の更新で変更または削除されることがあります。続行するには、この診断を非表示にします。
+
+[System.Diagnostics.CodeAnalysis.ExperimentalAttribute("MyID0001")]
+void sub()
+{
+    Console.WriteLine("The Philadelphia Experiment");
+}
